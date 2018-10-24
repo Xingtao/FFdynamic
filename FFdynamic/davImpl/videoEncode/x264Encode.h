285a30de9316f9da6803decc081fe5fd9b52ef58
pengxingtao <pengxingtao@kuaishou.com>
2018-10-24 10:44:44 +0800

    will use x264 do ROI encode

---
+++ FFdynamic/davImpl/videoEncode/x264Encode.h
@@ -0,0 +1,39 @@
+ #pragma once
+
+#include "ffmpegHeaders.h"
+#include "davImpl.h"
+#include "scaleFilter.h"
+
+namespace ff_dynamic {
+
+class FFmpegVideoEncode : public DavImpl {
+public:
+    FFmpegVideoEncode(const DavWaveOption & options) : DavImpl(options) {
+        implDefaultInstantiate();
+    }
+    virtual ~FFmpegVideoEncode() {onDestruct();}
+
+private: /* data process */
+    FFmpegVideoEncode(const FFmpegVideoEncode &) = delete;
+    FFmpegVideoEncode & operator= (const FFmpegVideoEncode &) = delete;
+    virtual int onConstruct();
+    virtual int onDestruct();
+    virtual int onProcess(DavProcCtx & ctx);
+    virtual int onDynamicallyInitializeViaTravelStatic(DavProcCtx & ctx);
+    virtual int onProcessTravelDynamic(DavProcCtx & ctx) {return 0;}
+    int dynamicallyInitialize(const DavImplTravel::TravelStatic & in);
+    int setupScaleFilter(const DavImplTravel::TravelStatic & in, const DavImplTravel::TravelStatic & out);
+    int receiveEncodeFrames(DavProcCtx & ctx);
+
+private: /* event process */
+    // int keyFrameRequest(const DavEvent & event);
+
+private:
+    AVCodecContext *m_encCtx = nullptr;
+    ScaleFilter *m_scaleFilter = nullptr;
+    ScaleFilterParams m_sfp;
+    uint64_t m_discardFrames = 0;
+    bool m_bForcedKeyFrame = false;
+};
+
+} //namespace ff_dynamic

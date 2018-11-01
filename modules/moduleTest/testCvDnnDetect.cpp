6afdd00a6b00d5e747e116e7b0accc6bd5d0cbe4
Xingtao <pengxingtao@kuaishou.com>
2018-11-01 23:30:49 +0800

    a waveSrc waveSink needed

--- modules/moduleTest/testCvDnnDetect.cpp
+++ modules/moduleTest/testCvDnnDetect.cpp
@@ -16,48 +16,57 @@ int main(int argc, char **argv) {
     // 1. create demux and get stream info
     DavWaveOption demuxOption((DavWaveClassDemux()));
     demuxOption.set(DavOptionInputUrl(), argv[1]);
-    // 2. video decode
+    // 2. video/audio decode
     DavWaveOption videoDecodeOption((DavWaveClassVideoDecode()));
     DavWaveOption audioDecodeOption((DavWaveClassAudioDecode()));
-    // 4. video encode
+    // 3. video encode
     DavWaveOption videoEncodeOption((DavWaveClassVideoEncode()));
     videoEncodeOption.setVideoSize(1280, 720);
     videoEncodeOption.set("preset", "veryfast");
+    // 4. audio encode
+    DavWaveOption audioEncodeOption((DavWaveClassAudioEncode()));
     // 5. mux
     DavWaveOption muxOption((DavWaveClassMux()));
     muxOption.set(DavOptionOutputUrl(), "dynaDetect.flv");
 
-    // 7. dynamic dnn detection streamlet
+    // 6. dynamic dnn detection streamlet
+    DavWaveOption cvDnnDetectOption((DavWaveClassCvDnnDetect()));
+    cvDnnDetectOption.set(DavOptionImplType(), "auto");
+    cvDnnDetectOption.set("detector_type", );
+    cvDnnDetectOption.set("detector_framework_tag", );
+    cvDnnDetectOption.set("model_path", );
+    cvDnnDetectOption.set("config_path", );
+    cvDnnDetectOption.set("classname_path", );
+    cvDnnDetectOption.setInt("backend_id", );
+    cvDnnDetectOption.setInt("target_id", );
+    cvDnnDetectOption.setDouble("scale_factor", );
+    cvDnnDetectOption.setBool("swap_rb", );
+    cvDnnDetectOption.setInt("width", );
+    cvDnnDetectOption.setInt("height", );
+    cvDnnDetectOption.setDouble("conf_threshold", 0.7);
+    cvDnnDetectOption.setDoubleArray("means", {});
 
     ////////////////////////////////////////////////////////////////////////////
     DavDefaultInputStreamletBuilder inputBuilder;
     DavDefaultOutputStreamletBuilder outputBuilder;
-    DavSingleWaveStreamletBuilder singleWaveBuilder;
     CvDnnDetectStreamletBuilder cvDnnBuilder;
     DavStreamletOption inputOption;
-    DavStreamletOption mixOption;
     DavStreamletOption outputOption;
-    DavStreamletOption singleWaveOption;
+    DavStreamletOption cvDnnBuildOption;
     // start build
     inputOption.setInt(DavOptionBufLimitNum(), 25);
-    auto streamletInput = inputBuilder.build({demuxOption, videoDecodeOption},
+    auto streamletInput = inputBuilder.build({demuxOption, videoDecodeOption, audioDecodeOption},
                                              DavDefaultInputStreamletTag("input"), inputOption);
     CHECK(streamletInput != nullptr);
-    auto streamletMix = mixBuilder.build({videoMixOption}, DavMixStreamletTag("mix"), mixOption);
-    CHECK(streamletMix != nullptr);
-    auto streamletOutput = outputBuilder.build({videoEncodeOption, muxOption},
+    auto streamletOutput = outputBuilder.build({videoEncodeOption, audioEncodeOption, muxOption},
                                                DavDefaultOutputStreamletTag("output"));
     CHECK(streamletOutput != nullptr);
-
-    singleWaveOption.setCategory(DavOptionInputDataTypeCategory(), DavDataInVideoRaw());
-    singleWaveOption.setCategory(DavOptionOutputDataTypeCategory(), DavDataOutVideoRaw());
-    auto dehazeStreamlet = singleWaveBuilder.build({videoDehazeOption},
-                                                   DavSingleWaveStreamletTag("dehaze"), singleWaveOption);
+    // TODO:
+    auto cvDnnStreamlet = cvDnnBuilder.build({videoDehazeOption},
+                                              DavSingleWaveStreamletTag("dehaze"), singleWaveOption);
     /* connect streamlets */
-    streamletInput >> streamletMix;
-    streamletInput >> dehazeStreamlet;
-    dehazeStreamlet >> streamletMix;
-    streamletMix >> streamletOutput;
+    streamletInput >> cvDnnStreamlet;
+    cvDnnStreamlet >> streamletOutput;
 
     // start
     DavRiver river({streamletInput, dehazeStreamlet, streamletMix, streamletOutput});
